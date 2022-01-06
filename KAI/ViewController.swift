3b034aeb0b95fc64c78a5337a4a676f4ddc1bf1d
zm <zhangmeng067@163.com>
2022-01-06 16:11:39 +0800

    update

---
+++ KAI/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KAI
+//
+//  Created by zm on 2021/12/31.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
