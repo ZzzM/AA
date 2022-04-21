14484806f95863007d1864904e6408e2631756f8
zm <zhangmeng067@163.com>
2022-04-21 11:51:34 +0800

    Scripts

---
+++ scripts/changelogs.sh
@@ -0,0 +1,36 @@
+function transform() {
+    local changelog=`cat $1`
+    changelog=${changelog%%---*}
+    changelog=${changelog#*###}
+    echo "###$changelog"
+}
+
+
+function sparkle_enclosure() {
+    local signature=`$PWD/assets/sign_update -s $SPARKLE_KEY $APP_NAME.dmg`
+    local enclosure="
+    <enclosure 
+    url=\"$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/releases/download/$APP_VERSION/$APP_NAME.dmg\" 
+    sparkle:version=\"$APP_BUILD\" 
+    sparkle:shortVersionString=\"$APP_VERSION\" 
+    $signature
+    type=\"application/octet-stream\" 
+    />
+    "
+    echo $enclosure
+}
+
+function release_changlogs() {
+    local flag="<?"
+    local all = "
+    $(transform CHANGELOG.md)
+    $flag
+    $(transform CHANGELOG_SC.md)
+    $flag
+    $sparkle_enclosure
+    "
+    echo $all > Release.md
+}
+
+release_changlogs
+
